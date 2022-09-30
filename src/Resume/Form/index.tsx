1c9b359ab35a59d7dfca05a0e04d71afbf2d1b53
Shahid <shahid.alam57@gmail.com>
2022-09-30 20:53:18 +0530

    added stepper

--- src/Resume/Form/index.tsx
+++ src/Resume/Form/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Stepper from '../../Stepper/Stepper'
+import StepperControl from '../../Stepper/StepperControl'
 const index = () => {
   return (
-    <div>index</div>
+    <div className="mx-auto rounded-2xl bg-white pb-2 shadow-xl md:w-1/2">
+    {/* Stepper */}
+    <div className="horizontal container mt-5 ">
+        <Stepper/>
+    </div>
+    {/**navigation control */}
+    <StepperControl/>
+  </div>
   )
 }
 
