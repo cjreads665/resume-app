91ec4482e69004fb72124025279f41a7cea06683
Shahid <shahid.alam57@gmail.com>
2022-12-20 21:46:22 +0530

    added api for home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import Header from "./Header";
 import Home from './Home';
 import Form from './Resume/Form'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
+import axios from "axios";
 function App() {
+  const api = {
+    dev: "http://localhost:8080",
+    live: "https://resume-back.onrender.com",
+  };
+
+  axios.get(api.live)
+  
   return (
     <div className="App font-[Poppins] p-4 min-h-screen">
       <BrowserRouter>
